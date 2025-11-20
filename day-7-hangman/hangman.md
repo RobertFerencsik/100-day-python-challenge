6166277cca0ca1bca9e406a638dd1994647cde2d
Robert Ferencsik <robert.ferencsik@gmail.com>
2025-11-20 11:21:35 +0100

    feat: Add day 7 hangman

--- day-7-hangman/hangman.md
+++ day-7-hangman/hangman.md
@@ -5,3 +5,4 @@ Your goal is to build a Hangman game using everything you have learnt about Pyth
 ## Knowledge to Use
 
 - All the knowledge form previous tasks, and also google (or generative ai???) is your friend
+- Importing other py files (here is not necessary to use __init__.py but it is good to know about it)
---
+++ day-7-hangman/main.py
@@ -0,0 +1,53 @@
+import random
+from hangman_words import word_list
+from hangman_art import logo, stages
+
+lives = 6
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+#print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print(f"You already guessed the letter '{guess}' please try an other one")
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"The guessed letter ({guess}) is not in the word")
+        if lives == 0:
+            game_over = True
+            print(f"*******************YOU LOSE | the word was: {chosen_word}*******************")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(stages[lives])
