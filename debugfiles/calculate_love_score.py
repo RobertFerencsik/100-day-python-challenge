42f0133599ee832f711cc9a4d3d19709df414cd1
Robert Ferencsik <robert.ferencsik@gmail.com>
2025-11-20 15:00:50 +0100

    doc: Document the functions in calculate_love_score.py

--- debugfiles/calculate_love_score.py
+++ debugfiles/calculate_love_score.py
@@ -1,4 +1,14 @@
 def calculate_love_score(name1, name2):
+    """
+    Calculates the “love score” between two names 
+    by counting how many letters from the words “true” and “love” appear in those names.
+
+    Args:
+        name1 (str): The first name to calculate the love score for.
+        name2 (str): The second name to calculate the love score for.
+    Returns:
+        int: The love score between the two names.
+    """
     names = [name1, name2]
     words = ["true", "love"]
     lambda x: names[x].tolower()
@@ -7,13 +17,23 @@ def calculate_love_score(name1, name2):
     for name in names:
         for word in words:
             sum_of_matching_chars += calculate_matching_chars(name, word)
-    print(sum_of_matching_chars)
+    #print(sum_of_matching_chars)
+    return sum_of_matching_chars
     
 def calculate_matching_chars(name, word):
+    """
+    Calculates the number of matching characters between a name and a word.
+
+    Args:
+        name (str): The name to calculate the matching characters for.
+        word (str): The word to calculate the matching characters for.
+    Returns:
+        int: The number of matching characters between the name and the word.
+    """
     sum = 0
     for char in name:
         if char in word:
            sum += 1 
     return sum
     
-calculate_love_score("Kanye West", "Kim Kardashian")
+print(calculate_love_score("Kanye West", "Kim Kardashian"))
